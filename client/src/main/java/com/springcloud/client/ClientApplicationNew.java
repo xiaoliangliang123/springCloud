38e55dd8e4dd8247f8b29e0bbe5a1a197bd04ba6
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-04-04 15:31:00 +0800

    修改ribbon fegin 分离

--- client/src/main/java/com/springcloud/client/ClientApplicationNew.java
+++ client/src/main/java/com/springcloud/client/ClientApplicationNew.java
@@ -2,16 +2,17 @@ package com.springcloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
+
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
 public class ClientApplicationNew {
+
 
     @Bean
     @LoadBalanced
--- client/src/main/java/com/springcloud/client/controller/ClientController.java
+++ client/src/main/java/com/springcloud/client/controller/ClientController.java
@@ -1,18 +1,15 @@
 package com.springcloud.client.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.springcloud.client.service.ServiceARemoteAPI;
-import com.springcloud.client.service.ServiceBRemoteAPI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-@EnableHystrix
 @RequestMapping("/")
 public class ClientController {
 
@@ -25,14 +22,9 @@ public class ClientController {
     @Value("${serviceb.id}")
     String servicebId;
 
-    @Autowired
-    ServiceARemoteAPI serviceARemoteAPI;
-
-    @Autowired
-    ServiceBRemoteAPI serviceBRemoteAPI;
 
 
-    @HystrixCommand(fallbackMethod="getFallbackA")
+
     @RequestMapping("/ribbonInvoke/servicea")
     @ResponseBody
     public String ribbonInvokeServiceA(){
@@ -43,6 +35,7 @@ public class ClientController {
 
 
     public Object getFallbackA() {
+
         return "getFallbackA error";
     }
 
@@ -54,41 +47,5 @@ public class ClientController {
         return result;
     }
 
-
-    @RequestMapping("/feignInvoke/servicea")
-    @ResponseBody
-    public String feignInvokeServiceA(){
-
-        String result = serviceARemoteAPI.feignInvokeServiceA();
-        return result;
-    }
-
-
-    @RequestMapping("/feignInvoke/serviceb")
-    @ResponseBody
-    public String feignInvokeServiceB(){
-
-        String result = serviceBRemoteAPI.feignInvokeServiceB();
-        return result;
-    }
-
-
-
-    @RequestMapping("/feignInvoke/servicea/add")
-    @ResponseBody
-    public Integer feignInvokeServiceAAdd(){
-
-        Integer result = serviceARemoteAPI.invokeAdd(1,2);
-        return result;
-    }
-
-
-    @RequestMapping("/feignInvoke/serviceb/add")
-    @ResponseBody
-    public Integer feignInvokeServiceBAdd(){
-
-        Integer result = serviceBRemoteAPI.invokeAdd(5,6);
-        return result;
-    }
 
 }
