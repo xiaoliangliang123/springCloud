58b0512f26a906ccecbee621d4c41e49aadf05a1
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-04-04 09:47:32 +0800

    添加fegin

--- client/src/main/java/com/springcloud/client/controller/ClientController.java
+++ client/src/main/java/com/springcloud/client/controller/ClientController.java
@@ -1,15 +1,18 @@
 package com.springcloud.client.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springcloud.client.service.ServiceARemoteAPI;
 import com.springcloud.client.service.ServiceBRemoteAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
+@EnableHystrix
 @RequestMapping("/")
 public class ClientController {
 
@@ -29,12 +32,18 @@ public class ClientController {
     ServiceBRemoteAPI serviceBRemoteAPI;
 
 
+    @HystrixCommand(fallbackMethod="getFallbackA")
     @RequestMapping("/ribbonInvoke/servicea")
     @ResponseBody
     public String ribbonInvokeServiceA(){
 
         String result = restTemplate.getForEntity(serviceaId+"/invoke", String.class).getBody();
         return result;
+    }
+
+
+    public Object getFallbackA() {
+        return "getFallbackA error";
     }
 
     @RequestMapping("/ribbonInvoke/serviceb")
@@ -54,11 +63,31 @@ public class ClientController {
         return result;
     }
 
+
     @RequestMapping("/feignInvoke/serviceb")
     @ResponseBody
     public String feignInvokeServiceB(){
 
         String result = serviceBRemoteAPI.feignInvokeServiceB();
+        return result;
+    }
+
+
+
+    @RequestMapping("/feignInvoke/servicea/add")
+    @ResponseBody
+    public Integer feignInvokeServiceAAdd(){
+
+        Integer result = serviceARemoteAPI.invokeAdd(1,2);
+        return result;
+    }
+
+
+    @RequestMapping("/feignInvoke/serviceb/add")
+    @ResponseBody
+    public Integer feignInvokeServiceBAdd(){
+
+        Integer result = serviceBRemoteAPI.invokeAdd(5,6);
         return result;
     }
 
--- client/src/main/java/com/springcloud/client/service/ServiceBRemoteAPI.java
+++ client/src/main/java/com/springcloud/client/service/ServiceBRemoteAPI.java
@@ -4,11 +4,16 @@ package com.springcloud.client.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@FeignClient(value = "eureka-service-b")
+@FeignClient(value = "eureka-service-b" ,url="localhost:9003")
 public interface ServiceBRemoteAPI {
 
-    @RequestMapping("/feignInvoke/serviceb")
+    @RequestMapping("/invoke")
     String feignInvokeServiceB();
+
+    @RequestMapping("/invokeAdd")
+    Integer invokeAdd(@RequestParam("a") final int a, @RequestParam("b")  final int b);
+
 }
