91dd629b61a2ba8acf2d3c43add7a68862dafccb
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-08-29 19:44:31 +0800

    添加freemarker

---
+++ client/src/main/java/com/springcloud/client/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.springcloud.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author:WANGLIANG(XIAO)
+ * @Date: 2019/8/29 19:04
+ * @Description :主页面
+ */
+@Controller
+@RequestMapping("/main")
+public class IndexController {
+
+    public String main(){
+        return "";
+    }
+}
---
+++ client/src/main/resources/templates/main.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>主页面</title>
+</head>
+<body>
+<div>
+    <div><a href="#">服务A主页</a></div>
+    <div><a href="#">服务B主页</a></div>
+    <div><a href="#">登录页</a></div>
+</div>
+</body>
+</html>
