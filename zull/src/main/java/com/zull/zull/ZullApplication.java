e2540b628b7179b4b81e981b69c05108f5e5e9d9
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-09-18 11:02:49 +0800

    添加FILTER 功能

--- zull/src/main/java/com/zull/zull/ZullApplication.java
+++ zull/src/main/java/com/zull/zull/ZullApplication.java
@@ -1,9 +1,11 @@
 package com.zull.zull;
 
+import com.zull.zull.filter.LoginFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,5 +14,11 @@ public class ZullApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZullApplication.class, args);
     }
+
+    @Bean
+    public LoginFilter firstPreFilter(){
+        return new LoginFilter();
+    }
+
 
 }
