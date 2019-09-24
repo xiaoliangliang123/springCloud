6b275f66f5365f28835e4715fe8cbf00948cfb5f
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-09-24 18:37:16 +0800

    添加FILTER 功能

--- zull/src/main/java/com/zull/zull/filter/LoginFilter.java
+++ zull/src/main/java/com/zull/zull/filter/LoginFilter.java
@@ -3,6 +3,8 @@ package com.zull.zull.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,18 +26,34 @@ public class LoginFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        return true;
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String requestUrl = request.getRequestURL().toString();
+        if(requestUrl.endsWith("/main/login")){
+            return false;
+        }else if(requestUrl.endsWith("/main/mainList")){
+            return false;
+        }else {
+            return true;
+        }
+
     }
 
     @Override
     public Object run() throws ZuulException {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-
+        String token = request.getHeader("token");
         System.out.println(
                 "Pre Filter: Request Method : " + request.getMethod() + " " +
-                        "Request URL : " + request.getRequestURL().toString());
-
+                        "Request URL : " + request.getRequestURL().toString()
+        +"token :"+token);
+        if (StringUtils.isBlank(token)) {
+            // 过滤该请求，不对其进行路由
+            ctx.setSendZuulResponse(false);
+            //返回错误代码
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
         return null;
     }
 }
